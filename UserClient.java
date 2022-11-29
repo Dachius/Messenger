3db7b0cc46be48d40249d63cbcf404213968d995
Drew Nguyen <80549085+dnguyen224@users.noreply.github.com>
2022-11-28 20:07:11 -0600

    Update UserClient.java

--- UserClient.java
+++ UserClient.java
@@ -1,7 +1,21 @@
-//Drew Nguyen
-//Group project 
-//Prototype / Proof of Concept
-//Messaging application
+/*
+How to run code:
+
+(1) Open 3 terminals
+-1 terminal for Server
+-2 terminals for separate clients
+
+(2) For the server terminal
+run: 
+java Server [XXXX]
+X's being any integer between 0-9
+
+For the client terminal
+run:
+java UserClient [XXXX]
+X's being the previously listed integers from the server port
+
+*/
 
 import java.net.*;
 import java.io.*;
